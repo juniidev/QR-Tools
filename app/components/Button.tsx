a564c0e1b5bcb9ec15aec5c8b1b850eeb2b4999d
juniidev <hms745@gmail.com>
2025-08-31 23:36:29 -0600

    Initial commit: QR generator

---
+++ app/components/Button.tsx
@@ -0,0 +1,30 @@
+// // app/components/Button.tsx
+// "use client";
+
+// import React from "react";
+
+// interface ButtonProps {
+//   onClick: () => void;
+//   children: React.ReactNode;
+// }
+
+// const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+//   return (
+//     <button
+//       onClick={onClick}
+//       style={{
+//         padding: "10px 20px",
+//         fontSize: "16px",
+//         borderRadius: "6px",
+//         border: "none",
+//         backgroundColor: "#4267b2",
+//         color: "#fff",
+//         cursor: "pointer"
+//       }}
+//     >
+//       {children}
+//     </button>
+//   );
+// };
+
+// export default Button;
